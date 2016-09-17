3203f115c72441ab074b8eec1a9c896ca18e7978
choochoo <2017398956@qq.com>
2016-09-18 00:49:18 +0800

    修改activity生命周期监听函数

--- src/main/java/com/nfl/libraryoflibrary/utils/CustomActivityLifecycleCallbacks.java
+++ src/main/java/com/nfl/libraryoflibrary/utils/CustomActivityLifecycleCallbacks.java
@@ -5,12 +5,15 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.nfl.libraryoflibrary.R;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -22,8 +25,9 @@ import java.lang.reflect.Method;
 @TargetApi(15)
 public class CustomActivityLifecycleCallbacks implements Application.ActivityLifecycleCallbacks{
 
-    private ViewGroup rootView ;
-
+    private String activityName ;
+    private int viewLevel = 2 ;// 默认为2
+    private ViewGroup rootView ;// DecorView
     private Context context ;
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
@@ -38,10 +42,10 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
     @Override
     public void onActivityResumed(final Activity activity) {
         context = activity ;
-        LogTool.i("ClassName:" + activity.getComponentName().getClassName()) ;
-        ViewGroup view = (ViewGroup) activity.getWindow().getDecorView() ;
-        rootView = view ;
-        LogTool.i("DecorView:" + view.getClass()) ;
+        activityName = activity.getComponentName().getClassName() ;
+        LogTool.i("ActivityName:" + activityName) ;
+        rootView = (ViewGroup) activity.getWindow().getDecorView() ;
+
 //        view.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
 //            public boolean onTouch(View v, MotionEvent event) {
@@ -50,11 +54,32 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
 //                return false;
 //            }
 //        });
-        if(null != view){
-            logViewsInfo(view);
+        if(null != rootView){
+            logViewsInfo(rootView);
+            viewLevel = 2 ;// logViewsInfo(View view)会改变viewLevel的值，这里恢复默认值
         }else{
-            LogTool.i("DecorView == null") ;
+            LogTool.i(activityName + "'s DecorView is null.") ;
         }
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
 
     }
 
@@ -63,7 +88,7 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
 //            if(view.getClass().isAssignableFrom(ViewGroup.class)){
 //                return true ;
 //            }
-            LogTool.i(ViewGroup.class + "|------s------|" + view.getClass().toString()) ;
+//            LogTool.i(ViewGroup.class + "|------s------|" + view.getClass().toString()) ;
             if(view instanceof ViewGroup){
                 return true ;
             }
@@ -71,23 +96,40 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
         return false ;
     }
 
+    /**
+     * 打印view的信息
+     * @param viewGroup
+     */
     private void logViewsInfo(ViewGroup viewGroup){
+        if(2 == viewLevel){
+            viewGroup.setTag(R.id.id_view_level_tag_key , 2);
+            viewGroup.setTag(R.id.id_view_parent_tag_key , 0);
+            LogTool.i("|-DecorView:" + rootView.getClass()) ;
+        }
         int count = viewGroup.getChildCount() ;
         if(count > 0 ){
             for (int i = 0 ; i < count ; i++){
                 View view = viewGroup.getChildAt(i) ;
+                view.setTag(R.id.id_view_level_tag_key , (Integer) viewGroup.getTag(R.id.id_view_level_tag_key) + 1);
+                String logHead = viewGroup.getTag(R.id.id_view_parent_tag_key) + "|";
+                for(int l = 0 ; l < (Integer) view.getTag(R.id.id_view_level_tag_key) ; l++){
+                    logHead += "-" ;
+                }
+                LogTool.i(logHead + view.getClass().toString()) ;
                 if(isViewGroup(view)){
-                    LogTool.i("ViewGroup:" + view.getClass().toString()) ;
+                    viewGroup.setTag(R.id.id_view_parent_tag_key , i);
+
+//                    LogTool.i("ViewGroup:" + view.getClass().toString()) ;
+//                    view.setTag(R.id.id_tag_key , viewLevel );
                     logViewsInfo((ViewGroup) view);
                 }else{
                     if(canResetOnClickListener(view)){
                         setClickListener(view);
                     }
-                    LogTool.i("===" + view.getClass().toString()) ;
                 }
             }
+            viewLevel++ ;
         }
-
     }
 
     private boolean canResetOnClickListener(View view){
@@ -144,7 +186,6 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
         return null ;
     }
 
-
     /**
      * 通过反射获取View的OnClickListener方法
      * @param view
@@ -190,41 +231,22 @@ public class CustomActivityLifecycleCallbacks implements Application.ActivityLif
             LogTool.i("是否有监听：" + hasOnClick);
             if (hasOnClick) {
                 final View.OnClickListener listener = getOnClickListener(view);
-                    if ((listener instanceof View.OnClickListener)) {
-                        view.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                if (null != listener) {
+                if ((listener instanceof View.OnClickListener)) {
+                    view.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (null != listener) {
 //                                    LogTool.i("复写了OnClickListener方法:" + );
 //                                    v.setTag();
-                                    // 为了解决activity视图改变，重新遍历
-                                    logViewsInfo(rootView);
-                                    listener.onClick(v);
-                                }
+                                // 为了解决activity视图改变，重新遍历
+                                logViewsInfo(rootView);
+                                listener.onClick(v);
                             }
-                        });
-                    }
+                        }
+                    });
+                }
             }
         }
     }
 
-    @Override
-    public void onActivityPaused(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-
-    }
 }
