b4ea69780ccf74f4c12ff21a1a91756a8dd43c8c
choochoo <2017398956@qq.com>
2016-09-28 15:15:24 +0800

    ToastTool多线程问题待解决

--- src/main/java/com/nfl/libraryoflibrary/utils/ToastTool.java
+++ src/main/java/com/nfl/libraryoflibrary/utils/ToastTool.java
@@ -7,6 +7,8 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
+import android.os.MessageQueue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,78 +25,84 @@ import com.nfl.libraryoflibrary.constant.ApplicationContext;
  */
 public class ToastTool {
 
-    private static Toast toast ;
-    private static Looper mainLooper ;
-    private static Looper myLooper ;
+    private static Toast toast;
+    private static boolean showEveryToast = false;// 是否显示每个Toast，即：一个toast显示完，另一个再显示；默认false，即：实时更新toast的显示内容，更新后toast的显示时间重置
 
-    public static void showShortToast(String info){
-        new ToastCreater(Toast.LENGTH_SHORT , info) ;
+    public static void showShortToast(String info) {
+        new ToastCreater(Toast.LENGTH_SHORT, info);
     }
 
-    public static void showShortToast(int resId){
-        new ToastCreater(Toast.LENGTH_SHORT , resId) ;
+    public static void showShortToast(int resId) {
+        new ToastCreater(Toast.LENGTH_SHORT, resId);
     }
 
-    public static void showLongToast(String info){
-        new ToastCreater(Toast.LENGTH_LONG , info) ;
+    public static void showLongToast(String info) {
+        new ToastCreater(Toast.LENGTH_LONG, info);
     }
 
-    public static void showCustomShortToast(String info){
-        new ToastCreater(ToastType.customShortToast , Toast.LENGTH_SHORT , info) ;
+    public static void showCustomShortToast(String info) {
+        new ToastCreater(ToastType.customShortToast, Toast.LENGTH_SHORT, info);
     }
 
-    public static void showCustomLongToast(String info){
-        new ToastCreater(ToastType.customShortToast , Toast.LENGTH_LONG , info) ;
+    public static void showCustomLongToast(String info) {
+        new ToastCreater(ToastType.customShortToast, Toast.LENGTH_LONG, info);
     }
 
-    public static void testToast(){
-        new ToastCreater(ToastType.testToast , Toast.LENGTH_SHORT , "") ;
+    public static void testToast() {
+        new ToastCreater(ToastType.testToast, Toast.LENGTH_SHORT, "");
     }
 
-    public static void showCustomTimeToast(View view , int time){
-            new ToastCreater(view , time) ;
+    public static void showCustomTimeToast(View view, int time) {
+        new ToastCreater(view, time);
     }
 
-    private static class ToastCreater{
+    private static class ToastCreater {
 
-        private ToastCreater(){
-            mainLooper = Looper.getMainLooper() ;
+        private Looper myLooper;
+        private boolean hasMyLooperBefore = false; // ToastTool是否在Looper(包括mainLooper和自建的Looper)中使用
+
+        private ToastCreater() {
+            if (null == ApplicationContext.applicationContext)
+                return;
             myLooper = Looper.myLooper();
-            if(mainLooper != myLooper){
+            LogTool.i("myLooper:" + myLooper);
+            if (null == myLooper) {
+                hasMyLooperBefore = false;
                 Looper.prepare();
                 myLooper = Looper.myLooper();
+            } else {
+                hasMyLooperBefore = true;
             }
-            if(null == ApplicationContext.applicationContext)
-                return;
-            if(null != toast){
+
+            if (null != toast && !showEveryToast) {
                 toast.cancel();
             }
         }
 
-        public ToastCreater(int toastLength , String info){
-            this() ;
-            toast = Toast.makeText(ApplicationContext.applicationContext , info , toastLength) ;
+        public ToastCreater(int toastLength, String info) {
+            this();
+            toast = Toast.makeText(ApplicationContext.applicationContext, info, toastLength);
             executeAndQuit();
         }
 
-        public ToastCreater(int toastLength , int resId){
-            this() ;
-            toast = Toast.makeText(ApplicationContext.applicationContext ,
-                    ApplicationContext.applicationContext.getResources().getText(resId) ,
-                    toastLength) ;
+        public ToastCreater(int toastLength, int resId) {
+            this();
+            toast = Toast.makeText(ApplicationContext.applicationContext,
+                    ApplicationContext.applicationContext.getResources().getText(resId),
+                    toastLength);
             executeAndQuit();
         }
 
-        public ToastCreater(ToastType toastType , int toastLength , String info){
-            this() ;
-            if(toastType == ToastType.customShortToast){
-                toast = Toast.makeText(ApplicationContext.applicationContext , info , toastLength) ;
-                ViewGroup v = (ViewGroup) toast.getView() ;
+        public ToastCreater(ToastType toastType, int toastLength, String info) {
+            this();
+            if (toastType == ToastType.customShortToast) {
+                toast = Toast.makeText(ApplicationContext.applicationContext, info, toastLength);
+                ViewGroup v = (ViewGroup) toast.getView();
                 v.setBackgroundResource(R.drawable.toast_bg);
                 ((TextView) v.getChildAt(0)).setTextColor(Color.WHITE);
-                int padding = ConvertTool.dp2px(5) ;
-                v.setPadding(padding , padding , padding , padding);
-            }else if(toastType == ToastType.testToast){
+                int padding = ConvertTool.dp2px(5);
+                v.setPadding(padding, padding, padding, padding);
+            } else if (toastType == ToastType.testToast) {
                 TextView message = new TextView(ApplicationContext.applicationContext);
                 message.setText("My Toast");
                 message.setBackgroundResource(R.drawable.toast_bg);
@@ -109,58 +117,73 @@ public class ToastTool {
             executeAndQuit();
         }
 
-        public ToastCreater(View view , int time){
-            this() ;
-            if(null == view){
-                CToast.makeText("自定义时长Toast" , time).show();
-            }else {
-                CToast cToast = CToast.makeText("" , time);
+        public ToastCreater(View view, int time) {
+            this();
+            if (null == view) {
+                CToast.makeText("自定义时长Toast", time).show();
+            } else {
+                CToast cToast = CToast.makeText("", time);
                 cToast.setmNextView(view);
                 cToast.show();
             }
-            if(mainLooper != myLooper && null != myLooper){
+            if (!hasMyLooperBefore) {
                 Looper.loop();
                 if (Build.VERSION.SDK_INT >= 18) {
                     myLooper.quitSafely();
-                }else {
+                } else {
                     myLooper.quit();
                 }
             }
         }
 
-        private void executeAndQuit(){
-            toast.show();
-            if(mainLooper != myLooper && null != myLooper){
-                Looper.loop();
-                if (Build.VERSION.SDK_INT >= 18) {
-                    myLooper.quitSafely();
-                }else {
-                    myLooper.quit();
+        private void executeAndQuit() {
+//            toast.show();
+            Handler handler = new Handler(myLooper, new Handler.Callback() {
+                @Override
+                public boolean handleMessage(Message msg) {
+                    if(msg.what == 1){
+                        toast.show();
+                    }
+                    return false;
                 }
+            });
+            handler.sendEmptyMessage(1) ;
+//            myLooper.getQueue().addIdleHandler(null);
+            if (!hasMyLooperBefore && null != myLooper) {
+                Looper.loop();
+                // 注意：写在Looper.loop()之后的代码不会被执行，
+                // 这个函数内部应该是一个循环，当调用mHandler.getLooper().quit()后，
+                // loop才会中止，其后的代码才能得以运行。
+
+//                if (Build.VERSION.SDK_INT >= 18) {
+//                    myLooper.quitSafely();
+//                }else {
+//                    myLooper.quit();
+//                }
             }
         }
     }
 
-    private enum ToastType{
-        customShortToast , testToast
+    private enum ToastType {
+        customShortToast, testToast
     }
 
     private static class CToast {
 
         private final Handler mHandler = new Handler();
-        private int mDuration = 2000 ;// 默认时长
+        private int mDuration = 2000;// 默认时长
         private int mGravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
-        private int mX = 0 , mY = 0 ;
-        private float mHorizontalMargin = 0.0f ;
-        private float mVerticalMargin = 0.0f ;
+        private int mX = 0, mY = 0;
+        private float mHorizontalMargin = 0.0f;
+        private float mVerticalMargin = 0.0f;
 
         private View mView;
         private View mNextView;
         private WindowManager mWM;
-        private WindowManager.LayoutParams mParams ;
+        private WindowManager.LayoutParams mParams;
 
         private CToast() {
-            mParams = new WindowManager.LayoutParams() ;
+            mParams = new WindowManager.LayoutParams();
             mParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
             mParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
             mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
