ffda27c489d245160eaec36d3227fd28d54a724c
choochoo <2017398956@qq.com>
2017-04-10 21:45:02 +0800

    update

--- src/main/java/com/nfl/libraryoflibrary/view/recyclerview/RecyclereViewBaseAdapter.java
+++ src/main/java/com/nfl/libraryoflibrary/view/recyclerview/RecyclereViewBaseAdapter.java
@@ -19,49 +19,20 @@ public abstract class RecyclereViewBaseAdapter<T extends RecyclereViewBaseAdapte
     private List<CustomRecyclerView.OnItemClickListener> onItemClickListenerList;
 
     @Override
-<<<<<<< HEAD
-    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (null == parent) {
-            customRecyclerView = (CustomRecyclerView) parent;
-=======
     public T onCreateViewHolder(ViewGroup parent, int viewType) {
         if (null != parent) {
             // 初始化
             customRecyclerView = (CustomRecyclerView) parent;
             onItemClickListenerList = customRecyclerView.getOnItemClickListenerList();
->>>>>>> 746daaf2f49056f09f0f3f588a5c7a63d42d4991
         }
-        return (BaseViewHolder) onCreateViewHolder2(parent, viewType);
+        return null;
     }
-
-    public abstract T onCreateViewHolder2(ViewGroup parent, int viewType);
 
     @Override
-<<<<<<< HEAD
-    public void onBindViewHolder(BaseViewHolder holder, int position) {
-        if (null != holder && null != holder.getItemView()) {
-            holder.getItemView().setOnClickListener(null);
-            holder.getItemView().setOnClickListener(new CustomOnClickListener<Integer>(position) {
-                @Override
-                public void onClick(View view, Integer integer) {
-                    super.onClick(view, integer);
-                    if (null != customRecyclerView.getOnItemClickListener()) {
-                        customRecyclerView.getOnItemClickListener().onClick(view, integer);
-                    }
-                }
-            });
-        }
-        onBindViewHolder2((T) holder, position);
-    }
-
-    public abstract void onBindViewHolder2(T holder, int position);
-
-=======
     public void onBindViewHolder(T holder, int position) {
         holder.getCustomOnClickListener().setT(position);
     }
 
->>>>>>> 746daaf2f49056f09f0f3f588a5c7a63d42d4991
     public class BaseViewHolder extends RecyclerView.ViewHolder {
 
         private View itemView;
