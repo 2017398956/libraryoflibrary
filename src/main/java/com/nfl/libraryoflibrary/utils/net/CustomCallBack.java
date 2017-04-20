09f6ca481259c8e790e515369cadcb0f2462fc53
choochoo <2017398956@qq.com>
2017-04-20 18:04:59 +0800

    网络访问工具的修改（线程间通讯）

--- src/main/java/com/nfl/libraryoflibrary/utils/net/CustomCallBack.java
+++ src/main/java/com/nfl/libraryoflibrary/utils/net/CustomCallBack.java
@@ -4,7 +4,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.support.annotation.UiThread;
 
 import com.google.gson.Gson;
 import com.nfl.libraryoflibrary.utils.ExceptionTool;
@@ -45,10 +44,12 @@ public abstract class CustomCallBack<T> implements CustomCallBackInterface<T>, C
             }
             if(null != myLooper && myLooper != Looper.getMainLooper()){
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                    myLooper.quitSafely();
+                    // myLooper.quitSafely();
+                    myLooper.quit();
                 }else {
                     myLooper.quit();
                 }
+                // myLooper = null ;
             }
         }
     };
@@ -102,20 +103,27 @@ public abstract class CustomCallBack<T> implements CustomCallBackInterface<T>, C
     @Override
     public void onFailure(Call call, IOException e) {
         failure();
-        if(null == myLooper){
-            Looper.prepare();
-            myLooper = Looper.myLooper() ;
-        }
-        Message msg = new Message();
-        msg.what = FAILURE_ON_MAIN_THREAD;
-        handler.sendMessage(msg);
+        if (null == Looper.myLooper() || Looper.myLooper() != Looper.getMainLooper()) {
+            LogTool.i("onFailure 方法在非 UI 线程");
+            looperPrepare();
+            Message msg3 = new Message();
+            msg3.what = FAILURE_ON_MAIN_THREAD;
+            handler.sendMessage(msg3);
         Looper.loop();
         LogTool.i("跳出 CustomCallBack Looper 3") ;
+        } else if (Looper.myLooper() == Looper.getMainLooper()) {
+            LogTool.i("onFailure 方法在 UI 线程");
+            failureOnMainThread();
+        }
     }
 
     @Override
     public void onResponse(Call call, Response response) throws IOException {
-
+        if (null == Looper.myLooper() || Looper.myLooper() != Looper.getMainLooper()) {
+            LogTool.i("onResponse 方法在非 UI 线程");
+        } else if (Looper.myLooper() == Looper.getMainLooper()) {
+            LogTool.i("onResponse 方法在 UI 线程");
+        }
         if(dismissProgressDialog()){
         CustomProgressBarDialog.dimissProgressBarDialog();
         }
@@ -137,33 +145,44 @@ public abstract class CustomCallBack<T> implements CustomCallBackInterface<T>, C
                 }
                 runOnSelfThread(t);
                 if (canRunOnMainThread) {
-                    if(null == myLooper){
-                        Looper.prepare();
-                        myLooper = Looper.myLooper() ;
-                    }
-                    Message msg = new Message();
-                    msg.what = RUN_ON_MAIN_THREAD;
-                    msg.obj = t;
-                    handler.sendMessage(msg);
+                    looperPrepare();
+                    Message msg1 = new Message();
+                    msg1.what = RUN_ON_MAIN_THREAD;
+                    msg1.obj = t;
+                    handler.sendMessage(msg1);
                     Looper.loop();
                     LogTool.i("跳出 CustomCallBack Looper 1") ;
                 }
             }
             if (canRunOnMainThread) {
-                if(null == myLooper){
-                    Looper.prepare();
-                    myLooper = Looper.myLooper() ;
-                }
-                Message msg = new Message();
-                msg.what = SUCCESS_ON_MAIN_THREAD;
-                msg.obj = resultTemp;
-                handler.sendMessage(msg);
+                looperPrepare();
+                Message msg2 = new Message();
+                msg2.what = SUCCESS_ON_MAIN_THREAD;
+                msg2.obj = resultTemp;
+                handler.sendMessage(msg2);
                 Looper.loop();
                 LogTool.i("跳出 CustomCallBack Looper 2") ;
             }
             canRunOnMainThread = false;
         }else {
             // success("访问服务器失败-->com.nfl.libraryoflibrary.utils.net.CustomCallBack");
+        }
+    }
+
+    /**
+     * 有时 myLooper 为 null 但 ，已经执行过 Looper.prepare() ;
+     * TODO 找出造成这种现象的原因
+     */
+    private void looperPrepare() {
+        try {
+            if(null == myLooper){
+                Looper.prepare();
+                myLooper = Looper.myLooper();
+            }
+        } catch (Exception e) {
+            LogTool.i("Looper 初始化异常:" + ExceptionTool.getExceptionTraceString(e));
+        } finally {
+            myLooper = Looper.myLooper();
         }
     }
 }
