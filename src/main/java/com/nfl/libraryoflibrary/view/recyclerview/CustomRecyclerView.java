2ea6c4bb0ef0d7c4c6bcb540c29579819abd031f
choochoo <2017398956@qq.com>
2017-06-07 13:45:17 +0800

    recyclerview添加上拉刷新

--- src/main/java/com/nfl/libraryoflibrary/view/recyclerview/CustomRecyclerView.java
+++ src/main/java/com/nfl/libraryoflibrary/view/recyclerview/CustomRecyclerView.java
@@ -6,13 +6,19 @@ package com.nfl.libraryoflibrary.view.recyclerview;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.nfl.libraryoflibrary.utils.ConvertTool;
+import com.nfl.libraryoflibrary.utils.LogTool;
 import com.nfl.libraryoflibrary.utils.TraceKeeper;
 
 import java.util.ArrayList;
@@ -24,9 +30,22 @@ import java.util.List;
 public class CustomRecyclerView<V extends RecyclerView.ViewHolder> extends RecyclerView {
 
     private Context context;
+    private boolean canLoadMore = false;// 是否开启加载更多功能
+    private ViewParent parentView;
+    private RecyclereViewBaseAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private CustomRecyclerViewDivider customRecyclerViewDivider;
 
     private List<OnItemClickListener> onItemClickListenerList;
+
+    private boolean hasStartAnim = false ;
+    private LoadMoreState loadMoreState = LoadMoreState.PREPARE;
+
+    public enum LoadMoreState {
+        STARTING, // 正在准备加载更多，此时还在拖拽状态
+        LOADING,  // 松开手指展示 加载更多 视图，且在加载完前不能修改
+        PREPARE      // 加载更多 准备阶段，即还没有被拖拽
+    }
 
     public CustomRecyclerView(Context context) {
         this(context, null);
@@ -36,9 +55,15 @@ public class CustomRecyclerView<V extends RecyclerView.ViewHolder> extends Recyc
         this(context, attrs, 0);
     }
 
-    public CustomRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+    public CustomRecyclerView(final Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context);
+        setListeners();
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
     }
 
     /**
@@ -60,13 +85,28 @@ public class CustomRecyclerView<V extends RecyclerView.ViewHolder> extends Recyc
         setLayoutManager(manager);
     }
 
+    private void setListeners() {
+        addOnScrollListener(onScrollListener);
+    }
+
     @Override
     public void setAdapter(Adapter adapter) {
         super.setAdapter(adapter);
+        this.adapter = (RecyclereViewBaseAdapter) adapter;
+    }
+
+    /**
+     * @param adapter
+     * @param canLoadMore 是否能上拉加载更多
+     */
+    public void setAdapter(Adapter adapter, boolean canLoadMore) {
+        this.setAdapter(adapter);
+        this.canLoadMore = canLoadMore;
     }
 
     /**
      * 清空所有 onItemClickListener
+     *
      * @param onItemClickListener
      */
     public void addOnItemClickListener(OnItemClickListener onItemClickListener) {
@@ -92,5 +132,168 @@ public class CustomRecyclerView<V extends RecyclerView.ViewHolder> extends Recyc
 
     public CustomRecyclerViewDivider getCustomRecyclerViewDivider() {
         return customRecyclerViewDivider;
+    }
+
+    public void setSwipeRefreshLayoutRefreshing(boolean isRefreshing) {
+        if (null != swipeRefreshLayout) {
+            swipeRefreshLayout.setRefreshing(isRefreshing);
+        }
+    }
+
+    private OnScrollListener onScrollListener = new OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+            if (newState == SCROLL_STATE_DRAGGING) {
+                if (CustomRecyclerView.this.computeVerticalScrollOffset() > 0 && !canScrollVertically(1)) {
+                    // 说明滑到了底部
+                    if (null != adapter) {
+                        setOnDragListener(onDragListener);
+                        setOnTouchListener(onTouchListener);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+        }
+    };
+
+    private OnDragListener onDragListener = new OnDragListener() {
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            return false;
+        }
+    };
+
+    private OnTouchListener onTouchListener = new OnTouchListener() {
+
+
+        View footerView;
+        ViewGroup.LayoutParams layoutParams;
+        float downPointY = 0;
+        float heigtTemp = 0;// 为正则表示向上拖拽
+        int maxHeight = ConvertTool.dp2px(80);
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            LogTool.i("action:" + event.getAction());
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    LogTool.i("downPointY:" + downPointY);
+                    downPointY = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    downPointY = 0;
+                    hasStartAnim = false ;
+                    // 如果展示了 加载更多
+                    if (loadMoreState == LoadMoreState.STARTING) {
+                        loadMoreState = LoadMoreState.LOADING;
+                        footerView = getChildAt(getChildCount() - 1);
+                        if (null != footerView) {
+                            layoutParams = footerView.getLayoutParams();
+                            if (null != layoutParams && null != adapter ) {
+                                if(layoutParams.height < maxHeight){
+                                    // 拖拽距离不够不加载更多
+                                    finishLoadMore() ;
+                                }else {
+                                    // 拖拽距离够，加载更多
+                                    layoutParams.height = adapter.footerViewMeasureHeight > 0 ? adapter.footerViewMeasureHeight : maxHeight ;
+                                    footerView.setLayoutParams(layoutParams);
+                                    adapter.refreshFooterViewLoading();
+                                    startLoadMore();
+                                    // 模拟加载更多成功
+                                    postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            finishLoadMore();
+                                        }
+                                    } , 4000) ;
+                                }
+                                layoutParams = null;
+                            } else {
+                                footerView = null;
+                            }
+                        }
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    // 在没有捕获到 acton_down 事件时，以第一次 move 的位置作为起点
+                    // downPoinntY 在每次手指离开屏幕时都会重置为 0
+                    if (downPointY == 0) {
+                        downPointY = event.getRawY();
+                    }
+                    heigtTemp = downPointY - event.getRawY();
+                    if (isBottom()) {
+                        if (loadMoreState == LoadMoreState.PREPARE) {
+                            // 添加 footerView
+                            adapter.addFooterView();
+                            loadMoreState = LoadMoreState.STARTING ;
+                        } else if (loadMoreState == LoadMoreState.STARTING) {
+                            footerView = getChildAt(getChildCount() - 1);
+                            if (null != footerView) {
+                                layoutParams = footerView.getLayoutParams();
+                                if (null != layoutParams) {
+                                    layoutParams.height += heigtTemp;
+                                    // 当 footerView 的高度高于设置的最大高度并且还没有刷新状态时更新 footerView 显示
+                                    if(layoutParams.height > maxHeight && !hasStartAnim){
+                                        adapter.refreshFooterViewStarting();
+                                        hasStartAnim = true ;
+                                    }
+                                    // 当 footerView 的高度超过 3 倍最大高度时，限制 footerView 的高度
+                                    if (layoutParams.height > 3 * maxHeight) {
+                                        layoutParams.height = 3 * maxHeight;
+                                    }
+                                    footerView.setLayoutParams(layoutParams);
+                                    layoutParams = null;
+                                }
+                                footerView = null;
+                            }
+                        }
+                    }
+                    break;
+            }
+            return false;
+        }
+    };
+
+    /**
+     * 开始加载更多，网络请求等在这里处理
+     */
+    public void startLoadMore() {
+
+    }
+
+    /**
+     * 加载更多 成功或失败后关闭 footerView
+     */
+    public void finishLoadMore() {
+        loadMoreState = LoadMoreState.PREPARE ;
+        if(null != adapter){
+            adapter.closeFooterView();
+        }
+    }
+
+    private boolean isBottom() {
+        return CustomRecyclerView.this.computeVerticalScrollOffset() > 0 && !canScrollVertically(1);
+    }
+
+    public boolean isCanLoadMore() {
+        return canLoadMore;
+    }
+
+    public void setCanLoadMore(boolean canLoadMore) {
+        this.canLoadMore = canLoadMore;
+    }
+
+    public LoadMoreState getLoadMoreState() {
+        return loadMoreState;
+    }
+
+    public void setLoadMoreState(LoadMoreState loadMoreState) {
+        this.loadMoreState = loadMoreState;
     }
 }
--- src/main/java/com/nfl/libraryoflibrary/view/recyclerview/RecyclereViewBaseAdapter.java
+++ src/main/java/com/nfl/libraryoflibrary/view/recyclerview/RecyclereViewBaseAdapter.java
@@ -1,10 +1,18 @@
 package com.nfl.libraryoflibrary.view.recyclerview;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.nfl.libraryoflibrary.R;
 import com.nfl.libraryoflibrary.listener.CustomOnClickListener;
+import com.nfl.libraryoflibrary.utils.LogTool;
 
 import java.util.List;
 
@@ -17,13 +25,57 @@ public abstract class RecyclereViewBaseAdapter<T extends RecyclereViewBaseAdapte
 
     private CustomRecyclerView<RecyclerView.ViewHolder> customRecyclerView;
     private List<CustomRecyclerView.OnItemClickListener> onItemClickListenerList;
+    protected View footerView;
+    private ProgressBar progressbar;
+    private ImageView iv_arrow;
+    private TextView tv_info;
+    protected int footerViewMeasureHeight;
+    protected int VIEW_TYPE_ITEM = 0;
+    protected int VIEW_TYPE_FOOTER = 1;
+
+    private Animation mRotateUpAnim;
+    private Animation mRotateDownAnim;
+    private final int ROTATE_ANIM_DURATION = 180;
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
 
     @Override
     public T onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (null == mRotateUpAnim) {
+            mRotateUpAnim = new RotateAnimation(0.0f, -180.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+            mRotateUpAnim.setFillAfter(true);
+        }
+        if (null == mRotateDownAnim) {
+            mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+            mRotateDownAnim.setFillAfter(true);
+        }
+
         if (null != parent) {
             // 初始化
             customRecyclerView = (CustomRecyclerView<RecyclerView.ViewHolder>) parent;
             onItemClickListenerList = customRecyclerView.getOnItemClickListenerList();
+        }
+        if (null != customRecyclerView && footerView == null) {
+            footerView = LayoutInflater.from(customRecyclerView.getContext()).inflate(R.layout.view_recycler_view_footer, customRecyclerView, false);
+            footerView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            progressbar = (ProgressBar) footerView.findViewById(R.id.progressbar);
+            iv_arrow = (ImageView) footerView.findViewById(R.id.iv_arrow);
+            tv_info = (TextView) footerView.findViewById(R.id.tv_info);
+            ViewGroup.LayoutParams layoutParams = footerView.getLayoutParams();
+            if (null != layoutParams) {
+                footerViewMeasureHeight = layoutParams.height;
+                LogTool.i("footerViewMeasureHeight:" + footerViewMeasureHeight);
+                layoutParams.height = 0;
+                footerView.setLayoutParams(layoutParams);
+            }
         }
         return null;
     }
@@ -59,5 +111,51 @@ public abstract class RecyclereViewBaseAdapter<T extends RecyclereViewBaseAdapte
         }
 
     }
+
+    /**
+     * 自定义 footerView 时不要调用这个方法
+     */
+    protected void addFooterView() {
+        if (null != footerView) {
+            progressbar.setVisibility(View.INVISIBLE);
+            iv_arrow.setVisibility(View.VISIBLE);
+            tv_info.setText(R.string.xlistview_footer_hint_normal);
+        }
+    }
+
+    protected void refreshFooterViewStarting() {
+        if (null != footerView) {
+            progressbar.setVisibility(View.INVISIBLE);
+            iv_arrow.setVisibility(View.VISIBLE);
+            iv_arrow.startAnimation(mRotateUpAnim);
+            tv_info.setText(R.string.xlistview_footer_hint_ready);
+        }
+    }
+
+    protected void refreshFooterViewLoading() {
+        if (null != footerView) {
+            progressbar.setVisibility(View.VISIBLE);
+            iv_arrow.clearAnimation();
+            iv_arrow.setVisibility(View.INVISIBLE);
+            tv_info.setText(R.string.xlistview_header_hint_loading);
+        }
+    }
+
+    protected void closeFooterView() {
+        if (null != footerView) {
+            ViewGroup.LayoutParams layoutParams = footerView.getLayoutParams();
+            if (null != layoutParams) {
+                layoutParams.height = 0;
+                footerView.setLayoutParams(layoutParams);
+            }
+        }
+    }
+
+//    public void addFooterView(View view) {
+//        if (null != view) {
+//            footerView = view;
+//        }
+//        addFooterView();
+//    }
 
 }
