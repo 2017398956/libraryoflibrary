82b10a9125560cbe554bebf49f9d7d4c21916544
2017398956 <2017398956@qq.com>
2024-10-27 18:54:28 +0800

    update

--- src/main/java/com/nfl/libraryoflibrary/utils/WifiTool.java
+++ src/main/java/com/nfl/libraryoflibrary/utils/WifiTool.java
@@ -1,18 +1,42 @@
 package com.nfl.libraryoflibrary.utils;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiNetworkSpecifier;
+import android.net.wifi.WifiNetworkSuggestion;
+import android.os.Build;
+import android.os.PatternMatcher;
+import android.util.Log;
 
+import androidx.activity.ComponentActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by fuli.niu on 2016/8/11.
  */
 public class WifiTool {
+
+    private Context applicationContext;
     // 自动连接自己的wifi热点（该热点信息没保存到手机中的情况），省的每次手动操作了
     private String wifiName;// 自动连接某个wifi时的wifi名称
     private String wifiPw;// 自动连接某个wifi时的WiFi密码
@@ -28,18 +52,112 @@ public class WifiTool {
     WifiManager.WifiLock mWifiLock;
 
     public WifiTool(Context context) {
+        applicationContext = context.getApplicationContext();
         // 取得WifiManager对象
-        mWifiManager = (WifiManager) context
-                .getSystemService(Context.WIFI_SERVICE);
+        mWifiManager = (WifiManager) applicationContext.getSystemService(Context.WIFI_SERVICE);
         // 取得WifiInfo对象
         mWifiInfo = mWifiManager.getConnectionInfo();
-//        mWifiManager
+    }
+
+    private boolean hasRegisterWifiConnectReceiver = false;
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // FIXME:API 31 开始，不止STATUS_NETWORK_SUGGESTIONS_SUCCESS 这一个状态码表示成功
+            if (!Objects.equals(intent.getAction(), WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
+                return;
+            }
+            // do post connect processing here
+            Log.d("NFL", "has added network suggestions.");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                connectWifiBySsid();
+            }
+        }
+    };
+
+    private WeakReference<ComponentActivity> activityRef;
+
+    public WifiTool(ComponentActivity activity) {
+        this(activity.getApplicationContext());
+        this.activityRef = new WeakReference<>(activity);
+        activity.getLifecycle().addObserver(new DefaultLifecycleObserver() {
+            @Override
+            public void onDestroy(@NonNull LifecycleOwner owner) {
+                if (hasRegisterWifiConnectReceiver && activityRef != null && activityRef.get() != null) {
+                    activityRef.get().unregisterReceiver(broadcastReceiver);
+                }
+            }
+        });
     }
 
     public WifiTool(Context context, String wifiName, String wifiPw) {
         this(context);
         this.wifiName = wifiName;
         this.wifiPw = wifiPw;
+    }
+
+    public void connectWifi(String wifiName, String wifiPw) {
+        openWifi();
+        this.wifiName = wifiName;
+        this.wifiPw = wifiPw;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            WifiNetworkSuggestion networkSuggestion = new WifiNetworkSuggestion.Builder()
+                    .setSsid(wifiName)
+//                    .setPriority(0)
+//                    .setWpa3Passphrase("")
+                    .setIsAppInteractionRequired(true)
+                    .build();
+            List<WifiNetworkSuggestion> suggestions = new ArrayList<>();
+            suggestions.add(networkSuggestion);
+            int status = mWifiManager.addNetworkSuggestions(suggestions);
+            if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
+                // do error handling here
+                Log.e("NFL", "add network suggestions failed!!!");
+            }
+            // Optional (Wait for post connection broadcast to one of your suggestions)
+            IntentFilter intentFilter = new IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
+            hasRegisterWifiConnectReceiver = true;
+            if (activityRef != null && activityRef.get() != null) {
+                activityRef.get().registerReceiver(broadcastReceiver, intentFilter);
+            }
+        } else {
+            connectToWiFiBelowQ(wifiName, wifiPw);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void connectWifiBySsid() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        // FIXME：是否应该在添加建议wifi成功后执行连接wifi的操作
+        WifiNetworkSpecifier specifier = new WifiNetworkSpecifier.Builder()
+                .setSsidPattern(new PatternMatcher(this.wifiName, PatternMatcher.PATTERN_PREFIX))
+//                .setWpa3Passphrase(this.wifiPw)
+//                    .setBssidPattern(MacAddress.fromString("10:03:23:00:00:00"), MacAddress.fromString("ff:ff:ff:00:00:00"))
+                .build();
+        NetworkRequest networkRequest = new NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+//                .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .setNetworkSpecifier(specifier)
+                .build();
+        ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+                Log.d("NFL", "onAvailable$network");
+//                connectivityManager.unregisterNetworkCallback(networkCallback)
+                mWifiManager.disconnect();
+            }
+
+            @Override
+            public void onUnavailable() {
+                super.onUnavailable();
+                Log.d("NFL", "onUnavailable");
+            }
+        };
+        connectivityManager.requestNetwork(networkRequest, networkCallback);
     }
 
     // 打开wifi
@@ -88,7 +206,7 @@ public class WifiTool {
     }
 
     // 指定配置好的网络进行连接
-    public void connetionConfiguration(int index) {
+    public void connectNetByNetworkId(int index) {
         if (index > mWifiConfigurations.size()) {
             return;
         }
@@ -96,7 +214,26 @@ public class WifiTool {
         mWifiManager.enableNetwork(mWifiConfigurations.get(index).networkId, true);
     }
 
+    private final BroadcastReceiver wifiStateChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (Objects.requireNonNull(intent.getAction())) {
+                case WifiManager.SCAN_RESULTS_AVAILABLE_ACTION:
+                    mWifiList = mWifiManager.getScanResults();
+                    break;
+                case ConnectivityManager.CONNECTIVITY_ACTION:
+                    break;
+            }
+        }
+    };
     public void startScan() {
+        // 注册 WiFi 扫描结果、WiFi 状态变化的广播接收
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION); // 监听wifi扫描结果
+        intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION); // 监听wifi是开关变化的状态
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION); // 监听wifi是否连接成功的广播
+        applicationContext.registerReceiver(wifiStateChangeReceiver, intentFilter);
+
         mWifiManager.startScan();
         // 得到扫描结果
         mWifiList = mWifiManager.getScanResults();
@@ -144,11 +281,29 @@ public class WifiTool {
         return (mWifiInfo == null) ? "NULL" : mWifiInfo.toString();
     }
 
-    // 添加一个网络并连接
-    public void addNetWork(WifiConfiguration configuration) {
-        int wcgId = mWifiManager.addNetwork(configuration);
-        mWifiManager.enableNetwork(wcgId, true);
+    /**
+     * 根据已有配置信息接入某个wifi热点
+     */
+    private boolean addNetWork(WifiConfiguration config) {
+        WifiInfo wifiinfo = mWifiManager.getConnectionInfo();
+        if (null != wifiinfo) {
+            mWifiManager.disableNetwork(wifiinfo.getNetworkId());
+        }
+        boolean result;
+        if (config.networkId > 0) {
+            result = mWifiManager.enableNetwork(config.networkId, true);
+            mWifiManager.updateNetwork(config);
+        } else {
+            int i = mWifiManager.addNetwork(config);
+            result = false;
+            if (i > 0) {
+                mWifiManager.saveConfiguration();
+                return mWifiManager.enableNetwork(i, true);
+            }
+        }
+        return result;
     }
+
 
     // 断开指定ID的网络
     public void disConnectionWifi(int netId) {
@@ -162,10 +317,9 @@ public class WifiTool {
      *
      * @return
      */
-    public boolean connectToWiFi() {
+    private boolean connectToWiFiBelowQ(String wifiName, String wifiPw) {
         String wifiSSID = "\"" + wifiName + "\"";// 注意双引号
         String wifiPassWord = "\"" + wifiPw + "\"";// 注意双引号
-        openWifi();
         startScan();
         WifiConfiguration mWifiConfiguration = null;
         if (mWifiConfigurations != null) {
@@ -179,14 +333,69 @@ public class WifiTool {
             }
         }
         // 未保存过该WiFi的密码，则进行wifi信息的配置
-        mWifiConfiguration = new WifiConfiguration();
-        mWifiConfiguration.SSID = wifiSSID;
-        mWifiConfiguration.preSharedKey = wifiPassWord;
-        mWifiConfiguration.status = WifiConfiguration.Status.ENABLED;
-        mWifiConfiguration.hiddenSSID = false;
-        mWifiManager.addNetwork(mWifiConfiguration);
+        mWifiConfiguration = createWifiConfig(wifiName, wifiPw, WifiCipherType.WPA);
+        return addNetWork(mWifiConfiguration);
+    }
 
-        return mWifiManager.enableNetwork(mWifiConfiguration.networkId, true);
+    public enum WifiCipherType {
+        WEP, WPA, NO_PASS, INVALID
+    }
+
+    /**
+     * 判断wifi热点支持的加密方式
+     */
+    public WifiCipherType getWifiCipher(String capabilities) {
+        if (capabilities.isEmpty()) {
+            return WifiCipherType.INVALID;
+        } else if (capabilities.contains("WEP")) {
+            return WifiCipherType.WEP;
+        } else if (capabilities.contains("WPA") || capabilities.contains("WPA2")
+                || capabilities.contains("WPS")) {
+            return WifiCipherType.WPA;
+        } else {
+            return WifiCipherType.NO_PASS;
+        }
+    }
+
+
+    private WifiConfiguration createWifiConfig(String ssid, String password, WifiCipherType type) {
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+        config.SSID = "\"" + ssid + "\"";
+
+        if (type == WifiCipherType.NO_PASS) {
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        }
+
+        if (type == WifiCipherType.WEP) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        }
+
+        if (type == WifiCipherType.WPA) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            config.status = WifiConfiguration.Status.ENABLED;
+
+        }
+        return config;
     }
 
     /**
